c06d8aaeb60ea85d31f9998e0311751daa58f07e
nakanshsai <55914116+nakanshsai@users.noreply.github.com>
2019-09-30 02:26:39 -0700

    Add files via upload

---
+++ JspMvcWebApp/src/com/zensar/models/LoginBean.java
@@ -0,0 +1,29 @@
+package com.zensar.models;
+
+public class LoginBean {
+private String username;
+private String password;
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+//business logic method
+public boolean validateUser() {
+	 if(username!=null && password!=null && username.equals("akansh") && password.equals("123")) {
+		 return true;
+		 
+	 }
+	 else {return false;
+ }
+}
+
+
+}
